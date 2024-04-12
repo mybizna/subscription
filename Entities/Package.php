39ef23bca42724a76b991d36f6c7a400679b485b
Dedan <dedanirungu@gmail.com>
2024-04-12 11:15:46 +0300

    Add rights() method to Package and Subscription models

--- Entities/Package.php
+++ Entities/Package.php
@@ -79,6 +79,12 @@ class Package extends BaseModel
      */
     public function rights(): array
     {
+        $rights = parent::rights();
 
+        $rights['staff'] = ['view' => true];
+        $rights['registered'] = ['view' => true];
+        $rights['guest'] = [];
+
+        return $rights;
     }
 }
