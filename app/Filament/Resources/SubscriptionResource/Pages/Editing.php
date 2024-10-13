522f88b0a926d58335359efd7648fca8a3ea777e
Dedan <dedanirungu@gmail.com>
2024-10-13 16:41:32 +0300

    Release 2024.12

---
+++ app/Filament/Resources/SubscriptionResource/Pages/Editing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Subscription\Filament\Resources\SubscriptionResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\EditingBase;
+
+// Class List that extends ListBase
+class Editing extends EditingBase
+{
+}
---
+++ app/Filament/Resources/SubscriptionResource/Pages/Listing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Subscription\Filament\Resources\SubscriptionResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\ListingBase;
+
+// Class List that extends ListBase
+class Listing extends ListingBase
+{
+}
