9b5cffb18c3f96c5b51a9778365ec2ea84c2c489
Dedan <dedanirungu@gmail.com>
2025-01-23 03:42:32 +0300

    Make Filament Resource Shorter

--- app/Filament/Resources/PackageResource.php
+++ app/Filament/Resources/PackageResource.php
@@ -2,12 +2,7 @@
 
 namespace Modules\Subscription\Filament\Resources;
 
-use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Tables;
-use Filament\Tables\Table;
 use Modules\Base\Filament\Resources\BaseResource;
-use Modules\Subscription\Filament\Resources\PackageResource\Pages;
 use Modules\Subscription\Models\Package;
 
 class PackageResource extends BaseResource
@@ -20,80 +15,4 @@ class PackageResource extends BaseResource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('title')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('amount')
-                    ->numeric()
-                    ->default(null),
-                Forms\Components\Textarea::make('description')
-                    ->required()
-                    ->columnSpanFull(),
-                Forms\Components\TextInput::make('no_of_days')
-                    ->numeric()
-                    ->default(null),
-                Forms\Components\Toggle::make('recurrence'),
-                Forms\Components\Toggle::make('published'),
-                Forms\Components\TextInput::make('setup_fee')
-                    ->numeric()
-                    ->default(null),
-            ]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('title')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('amount')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('no_of_days')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\IconColumn::make('recurrence')
-                    ->boolean(),
-                Tables\Columns\IconColumn::make('published')
-                    ->boolean(),
-                Tables\Columns\TextColumn::make('setup_fee')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-            ])
-            ->filters([
-                Tables\Filters\TrashedFilter::make(),
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                    Tables\Actions\ForceDeleteBulkAction::make(),
-                    Tables\Actions\RestoreBulkAction::make(),
-                ]),
-            ]);
-    }
-
-    public static function getPages(): array
-    {
-
-        return [
-            'index' => Pages\Listing::route('/'),
-            'create' => Pages\Creating::route('/create'),
-            'edit' => Pages\Editing::route('/{record}/edit'),
-        ];
-    }
 }
--- app/Filament/Resources/SubscriptionResource.php
+++ app/Filament/Resources/SubscriptionResource.php
@@ -2,12 +2,7 @@
 
 namespace Modules\Subscription\Filament\Resources;
 
-use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Tables;
-use Filament\Tables\Table;
 use Modules\Base\Filament\Resources\BaseResource;
-use Modules\Subscription\Filament\Resources\SubscriptionResource\Pages;
 use Modules\Subscription\Models\Subscription;
 
 class SubscriptionResource extends BaseResource
@@ -19,99 +14,4 @@ class SubscriptionResource extends BaseResource
     protected static ?string $navigationGroup = 'Subscription';
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('amount')
-                    ->numeric()
-                    ->default(null),
-                Forms\Components\Toggle::make('completed'),
-                Forms\Components\Toggle::make('status'),
-                Forms\Components\DateTimePicker::make('expiry_date'),
-                Forms\Components\DateTimePicker::make('last_upgrade_date'),
-                Forms\Components\Textarea::make('param')
-                    ->columnSpanFull(),
-                Forms\Components\TextInput::make('payment_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\Toggle::make('successful'),
-                Forms\Components\DateTimePicker::make('upgrade_date'),
-                Forms\Components\TextInput::make('partner_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('package_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\Toggle::make('paid'),
-            ]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('amount')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\IconColumn::make('completed')
-                    ->boolean(),
-                Tables\Columns\IconColumn::make('status')
-                    ->boolean(),
-                Tables\Columns\TextColumn::make('expiry_date')
-                    ->dateTime()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('last_upgrade_date')
-                    ->dateTime()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('payment_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\IconColumn::make('successful')
-                    ->boolean(),
-                Tables\Columns\TextColumn::make('upgrade_date')
-                    ->dateTime()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('partner_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('package_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\IconColumn::make('paid')
-                    ->boolean(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-            ])
-            ->filters([
-                Tables\Filters\TrashedFilter::make(),
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                    Tables\Actions\ForceDeleteBulkAction::make(),
-                    Tables\Actions\RestoreBulkAction::make(),
-                ]),
-            ]);
-    }
-
-    public static function getPages(): array
-    {
-
-        return [
-            'index' => Pages\Listing::route('/'),
-            'create' => Pages\Creating::route('/create'),
-            'edit' => Pages\Editing::route('/{record}/edit'),
-        ];
-    }
 }
